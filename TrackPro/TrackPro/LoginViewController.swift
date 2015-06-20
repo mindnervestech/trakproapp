9b5abeb3e896559968dd6a49a7e6d5db2a5ae91f
Harry <Mindnervestech@Gmail.com>
2015-06-20 11:56:18 -0700

    v1

---
+++ TrackPro/TrackPro/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  JSONSample
+//
+//  Created by Harpreet Paul on 6/15/15.
+//  Copyright (c) 2015 MindNerves. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func login(sender: UIButton) {
+    //    var apiCred: ApiCredentials = ApiCredentials()
+     //   apiCred.username = "brisbane"
+      //  apiCred.password = "North170"
+       // WialonApiManager.sharedInstance.login(apiCred)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
